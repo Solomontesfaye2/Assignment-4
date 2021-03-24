a062d834bf388b540f5fa8cf881e37d775a51d03
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-24 20:27:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('bg')                .addEventListener('click' ,background)
+document.getElementById('change-text')       .addEventListener('click' ,report)
+
+function background () {
+  document.body.style.backgroundColor = 'tomato'
+}
+
+function report () {
+  document.getElementById('change-text').innerHTML = 'Report Successfully submitted thanks for your feedback'
+}
+  
